bff967b228270ba840b7a3c23b46f36b9ba14800
Matthew R <mattsdev107@gmail.com>
2020-11-16 18:48:12 -0600

    Add Shuffle Card Method
    Use the Fisher–Yates shuffle Algorithm for make a array to randomize the order

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -7,16 +7,18 @@ public class Deck{
 	
 	public Card[] deck;
 	public int[] randomDrawOrder;
-
+	public int currentCard;
 	private int numberOfCards = 52;
-	private System.Random rng = new System.Random();
 
 	public Deck() {
 		string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 		string[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+		
 		randomDrawOrder = Enumerable.Range(0, 52).ToArray();
 		deck = new Card[numberOfCards];
+		
 		int currentCard = 0;
+
 		for (int suitCount = 0; suitCount < suits.Length; suitCount++) {
 			for(int rankCount = 0; rankCount < ranks.Length; rankCount++) {
 				deck[currentCard] = new Card(ranks[rankCount], suits[suitCount]);
@@ -25,8 +27,16 @@ public class Deck{
 		}
 	}
 
-	public void SuffleDeck() {
+	public void ShuffleDeck() {
+		System.Random rand = new System.Random();
+		int n = randomDrawOrder.Length - 1;
 
+		for (int i = n; i > 0; i--) {
+			int j = rand.Next(0, i);
+			int temp = randomDrawOrder[i];
+			randomDrawOrder[i] = randomDrawOrder[j];
+			randomDrawOrder[j] = temp;
+		}
 	}
 
 	public void DrawCard (){
@@ -34,9 +44,18 @@ public class Deck{
 	}
 
 	public override string ToString() {
-		string output = "";
+		string output = null;
 		foreach(Card card in deck) {
-			output += (card.ToString() + ", ");
+			output += card.ToString() + ", ";
+		}
+		return output;
+	}
+
+	//? Don't forget to remove this method when done with Shuffle Deck function
+	public string RandomOrderToString() {
+		string output = null;
+		foreach(int num in randomDrawOrder) {
+			output += num.ToString() + " ";
 		}
 		return output;
 	}
